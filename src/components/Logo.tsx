0866c79b3a904ace8262043d986eb34e57305fc7
Colson <colsoncalifornication@gmail.com>
2023-08-22 12:48:33 +0545

    add logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div className="flex justify-center items-center space-x-2 mb-4">
+      <span className="text-gray-600 text-4xl font-bold">fun</span>
+      <span className="text-gray-600 text-4xl font-bold">PLAY</span>
+    </div>
+  );
+};
+
+export default Logo;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Logo from './components/Logo';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Logo />
     <App />
   </React.StrictMode>
 );
